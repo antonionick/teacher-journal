82c2dde1120b7e8db3b645e9abe3d8e1c302bfff
Anton Golovko <golovko639@gmail.com>
2020-05-08 12:36:28 +0300

    refactor: implement http tests for subject service

--- src/app/subjects/services/subject.service.spec.ts
+++ src/app/subjects/services/subject.service.spec.ts
@@ -1,7 +1,14 @@
 import { HttpClient } from '@angular/common/http';
+import { async, inject, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { SubjectService } from './subject.service';
 import { Subject } from '../../common/models/subject';
+import { urlProvider } from '../../url';
+import * as HttpUtils from  '../../common/utils/http';
+import { Options } from '../../common/models/utils';
+
+const { subjects: url } = urlProvider;
 
 describe('SubjectService', () => {
   describe('isChanged', () => {
@@ -39,5 +46,95 @@ describe('SubjectService', () => {
       const isChanged: boolean = service.isChanged(sourceSubject, subject);
       expect(isChanged).toBe(false);
     });
+  });
+
+  describe('Subject http', () => {
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule],
+        providers: [SubjectService],
+      }).compileComponents();
+    }));
+
+    it('should get one subject from server', inject(
+      [SubjectService, HttpTestingController],
+      (service: SubjectService, backend: HttpTestingController) => {
+        const subject: Subject = new Subject();
+        const httpOptions: Options = HttpUtils.getParamsWithId(1);
+
+        service.fetchSubject(httpOptions).subscribe((res) => {
+          expect(res).toEqual(subject);
+        });
+
+        backend.expectOne({
+          method: 'GET',
+          url: `${url}?id=1`,
+        }).flush([subject]);
+      },
+    ));
+
+    it('should get subjects from server', inject(
+      [SubjectService, HttpTestingController],
+      (service: SubjectService, backend: HttpTestingController) => {
+        const subjects: Array<Subject> = [new Subject()];
+
+        service.fetchSubjects().subscribe((res) => {
+          expect(res).toEqual(subjects);
+        });
+
+        backend.expectOne({
+          method: 'GET',
+          url,
+        }).flush(subjects);
+      },
+    ));
+
+    it('should add subject to server', inject(
+      [SubjectService, HttpTestingController],
+      (service: SubjectService, backend: HttpTestingController) => {
+        const subject: Subject = new Subject();
+
+        service.addSubjectServer(subject).subscribe((res) => {
+          expect(res).toEqual(subject);
+        });
+
+        backend.expectOne({
+          method: 'POST',
+          url,
+        }).flush(subject);
+      },
+    ));
+
+    it('should update subject in server', inject(
+      [SubjectService, HttpTestingController],
+      (service: SubjectService, backend: HttpTestingController) => {
+        const subject: Subject = new Subject({ id: 1 });
+
+        service.updateSubject(subject).subscribe((res) => {
+          expect(res).toEqual(subject);
+        });
+
+        backend.expectOne({
+          method: 'PUT',
+          url: `${url}/1`,
+        }).flush(subject);
+      },
+    ));
+
+    it('should delete subject in server', inject(
+      [SubjectService, HttpTestingController],
+      (service: SubjectService, backend: HttpTestingController) => {
+        const subject: Subject = new Subject({ id: 1 });
+
+        service.deleteSubject(subject).subscribe((res) => {
+          expect(res).toEqual({} as Subject);
+        });
+
+        backend.expectOne({
+          method: 'DELETE',
+          url: `${url}/1`,
+        }).flush({});
+      },
+    ));
   });
 });
